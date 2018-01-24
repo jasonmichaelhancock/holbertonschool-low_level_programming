05a0182a1a78d81d132ec4c3222b31ed8cda8116
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-24 00:37:06 +0000

    task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 
 /**
+ * main - Entry point
  *
- *main - entry point
- *
- *
- *return 0 - Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('H');
-    _putchar('o');
-    _putchar('l');
-    _putchar('b');
-    _putchar('e');
-    _putchar('r');
-    _putchar('t');
-    _putchar('o');
-    _putchar('n');
-    _putchar('\n');
-    return(0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
