b37f3d4f0eb79ffd40c17cb26fa389dc4cecfd97
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-24 23:29:42 +0000

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Entry point
+ * @n: the value to return
+ * @m: the value to return
+ *
+ * Return: int
+ */
+void jack_bauer(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 23; n++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar ('0' + (n / 10));
+			_putchar ('0' + (n % 10));
+			_putchar (':');
+			_putchar ('0' + (m / 10));
+			_putchar ('0' + (m % 10));
+			_putchar ('\n');
+		}
+	}
+}
