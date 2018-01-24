dcce23a413c6f609aa68d33b29577c8c49ec8aa8
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-24 00:44:01 +0000

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
 
+/**
+ * print_alphabet  - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a';c <= 'z';c++);
-    _putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
 }
