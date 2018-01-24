7829b557b42240bea23139c34be34f0ecb008bce
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-24 22:11:47 +0000

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - Entry point
+ * @int: the value to return
+ *
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n + '0');
+	}
+	else
+	{
+		return ((n * (-1)) + '0');
+	}
+}
