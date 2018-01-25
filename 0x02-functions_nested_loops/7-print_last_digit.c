e2c8efc327153539c827e5bf28618e3794a7256a
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-25 01:14:49 +0000

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,20 @@
  * print_last_digit - Entry point
  * @n: the value to return
  *
- * Return: int
+ * Return: n
  */
 int print_last_digit(int n)
 {
+	int a = n % 10;
+
 	if (n >= 0)
 	{
-		_putchar('0' + (n % 10));
+		_putchar('0' + a);
+		return ('a');
 	}
 	else
 	{
-		_putchar('0' + ((n * -1) % 10));
+		_putchar('0' + (a * -1));
+		return ('a' * -1);
 	}
-	return (n);
 }
