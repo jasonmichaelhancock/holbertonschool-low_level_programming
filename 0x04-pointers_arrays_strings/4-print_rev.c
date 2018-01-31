107b949e28ba7be2def652c251e85c9e5e921168
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-31 06:01:04 +0000

    task4

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print string
+ * @str: the array value
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+        {
+		;
+        }
+	for (length = (length - 2); s[length] >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
