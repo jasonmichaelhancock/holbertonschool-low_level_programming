cc492e0a8d5ffe99eecadf62699ae4d38a1b084e
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-31 00:23:16 +0000

    task1

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - reset to 98
+ * @a: one pointer value
+ * @b: second pointer value
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
