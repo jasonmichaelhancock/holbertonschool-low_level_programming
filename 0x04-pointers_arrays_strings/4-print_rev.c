df8d76f1f5ffcd53370dbf153e80b862db28cc42
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-31 21:15:22 +0000

    task4

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,7 @@ void print_rev(char *s)
 	int length;
 
 	for (length = 0; s[length] != '\0'; length++)
-	{
 		;
-	}
 	for (length = (length - 1); s[length] >= 0; length--)
 	{
 		_putchar(s[length]);
