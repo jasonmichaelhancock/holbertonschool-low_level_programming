e172beb3875f9b9a7d8023ca8f021f1400415820
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-01-31 05:03:13 +0000

    task7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -13,9 +14,18 @@ void puts_half(char *str)
 	for (length = 0; str[length] != '\0'; length++)
 	{
 	}
-	for (length = ((length / 2) + 1); str[length] != '\0'; length++)
+	printf("%d", length);
+	if (length % 2 == 0)
 	{
+		for (length = (length / 2); str[length] != '\0'; length++)
+		{
 		_putchar(str[length]);
+		}
 	}
+	else
+		for (length = ((length / 2) + 1); str[length] != '\0'; length++)
+		{
+			_putchar(str[length]);
+                }
 	_putchar('\n');
 }
