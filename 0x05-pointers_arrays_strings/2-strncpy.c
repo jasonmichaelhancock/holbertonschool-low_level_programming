6661fdaf541a279fec6f51b5630aa983b9c61242
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-04 23:56:42 +0000

    task2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
-
 /**
  * _strncpy - swap at a certain point
  * @dest: destination
  * @src: source
  * @n: max digits
- *
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -14,37 +12,28 @@ char *_strncpy(char *dest, char *src, int n)
 	int x;
 
 	while (src[i] != '\0')
-	{
 		i++;
-	}
+
 	x = i;
 
 	if (x < n)
 	{
-		i = 0;
-
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	}
-	if (x >= n)
-	{
-		i = 0;
-
-		while (src[i] != '\0')
+		for (i = 0; i <= x; i++)
 		{
 			if  (i < n)
-			{
 				dest[i] = src[i];
-				i++;
-			}
 			else
-			{
+				dest[i] = '\0';
+		}
+	}
+	else
+	{
+		for (i = 0; src[i] != '\0'; i++)
+		{
+			if  (i < n)
+				dest[i] = src[i];
+			else
 				dest[i] = '*';
-				i++;
-			}
 		}
 	}
 	return (dest);
