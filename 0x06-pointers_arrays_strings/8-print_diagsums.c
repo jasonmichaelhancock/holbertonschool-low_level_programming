3e8bc530a350060c29da2df05a60e50f121ca067
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-07 01:41:18 +0000

    task7

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum diagonals
+ * @a: value return
+ * @size: the square.
+ *
+ * Return: void.
+ */
+void print_diagsums(int *a, int size)
+{
+	int A = 0;
+	int B = 0;
+	int i;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		A = A + a[i];
+		i += size;
+	}
+	for (i = size - 1; i < (size * size); i--)
+	{
+		B = B + a[i];
+		i += size;
+	}
+	printf("%d, ", A);
+	printf("%d\n", B);
+}
