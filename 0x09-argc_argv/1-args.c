6bea2f59f3031d9e496976961917ff4ae81acabc
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-12 03:09:31 +0000

    task1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - the function
+ * @argc: number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0 is success.
+ */
+int main(int argc; char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		;
+	}
+	printf("%d\n", i);
+	return (0);
+}
