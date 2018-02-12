0164d8fa5c93c18c0af04e30aa5537e215f89651
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-12 23:59:34 +0000

    task3

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * main - the function
  * @argc: number of arguments
- * @argv: the array of arguments                                         *
+ * @argv: the array of arguments
+ *
  * Return: 0 is success.
  */
 int main(int argc, char **argv)
 {
-	int x = (*argv[1] * *argv[2]);
+	int x = ((atoi(argv[1])) * (atoi(argv[2])));
 
 	if (argc == 3)
 	{
