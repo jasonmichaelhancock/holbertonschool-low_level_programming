b0b0096f2d35aa47fea10f244174236994f400d4
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-17 00:14:50 +0000

    task1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -18,35 +18,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int k;
 	unsigned int l;
 
-	if (s1 == NULL)
-		s1 = ("");
-	if (s2 == NULL)
-		s2 = ("");
+        if (s1 == NULL)
+                s1 = ("");
+        if (s2 == NULL)
+                s2 = ("");
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		;
-	}
-	for (k = 0; s2[k] != '\0'; k++)
-	{
-		;
-	}
-	p = malloc((sizeof(char) * i) + (sizeof(char) * k) + 1);
-	if (p == 0)
-		return (NULL);
-	for (j = 0; j < i; j++)
-		p[j] = s1[j];
+        for (i = 0; s1[i] != '\0'; i++)
+        {
+                ;
+        }
+        for (k = 0; s2[k] != '\0'; k++)
+        {
+                ;
+        }
+        p = malloc((sizeof(char) * i) + (sizeof(char) * k) + 1);
+        if (p == 0)
+                return (NULL);
+        for (j = 0; j <= i + 1; j++)
+                p[j] = s1[j];
 	if (k < n)
-	{
-		for (l = 0; l <= k; l++)
-			p[l + i + 1] = s2[l];
+        {
+		for (l = 0; l <= k + 1; l++)
+			p[l + i] = s2[l];
 	}
 	else
-		for (l = 0; l < n; l++)
-		{
-			p[l + i + 1] = s2[l];
-		}
-	l++
-	p[l + i + 1] = '\0';
-	return (p);
+                for (l = 0; l < n; l++)
+                        p[l + i] = s2[l];
+        return (p);
 }
