2d0c2045796469c92f8b0890ed4ac97402404f15
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-17 01:43:09 +0000

    task2

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - callocs
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
