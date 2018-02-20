f49254d5a11a703012b6795e2af39e2683bd1fb3
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-20 04:05:45 +0000

    task0

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - initializing the sstruct.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: void.
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 #endif
