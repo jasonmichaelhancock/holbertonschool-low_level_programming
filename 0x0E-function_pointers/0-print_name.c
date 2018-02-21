a661635a280c4f2cc7262b0b6a44bff7363156d9
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-21 23:36:01 +0000

    task0

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -2,12 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
+ * @f: pointer to the function
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
+	return;
 }
