d2c82bd2a100949f1efc19c68854412230f51eff
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-21 01:58:08 +0000

    task1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -8,14 +8,14 @@
  * @age: the dog's age
  * @owner: the dog's owner
  *
- * Return: d.
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d !=0)
+	if (d != 0)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
+	};
 }
