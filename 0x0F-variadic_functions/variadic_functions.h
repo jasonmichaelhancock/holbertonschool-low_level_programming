1354ec0cfc4d5929c32226077e3f9ee8f359fcce
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-26 03:41:59 +0000

    task3

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VFUNC_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -15,7 +16,7 @@ void print_all(const char * const format, ...);
 typedef struct op
 {
         char *cifs;
-        void (*fp)(va_list thelist);
+        void (*fp)(va_list);
 } op_t;
 
 void op_string(va_list thelist);
