766d0d2a4d0c10eb97352adebaf8823344cf5ca5
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-02-26 03:34:48 +0000

    task3

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -17,7 +17,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void op_string(va_list thelist);
+/**                                                                                     
+ * struct op - Struct op                                                                
+ *                                                                                      
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+        char *cifs;
+        void (*fp)(va_list thelist);
+} op_t;void op_string(va_list thelist);
 void op_float(va_list thelist);
 void op_int(va_list thelist);
 void op_char(va_list thelist);
