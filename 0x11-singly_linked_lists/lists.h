e3e9b7f1fa511ad1b22b8c74a26504d5a4e04852
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-02 05:17:00 +0000

    task1

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -17,12 +17,6 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-typedef struct list_s
-{
-        char *str;
-        unsigned int len;
-        struct list_s *next;
-} list_t;
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
