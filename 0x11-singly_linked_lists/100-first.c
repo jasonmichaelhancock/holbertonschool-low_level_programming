0b7c85ee12e8c6365ef6b9eb8d628628b94bca85
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-02 22:03:16 +0000

    task5

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ */
+void myStartupFun (void)
+{
+        printf("You're beat! and yet, you must allow,\nI bore my house upon\
+ my back!\n");
+}
