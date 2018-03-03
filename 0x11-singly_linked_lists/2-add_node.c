d803bc68eed8f97fc767b2ae52b9692640e60ee0
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-03 00:54:40 +0000

    task2

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -13,23 +13,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	int i;
+	int i = 0;
 
-	if (*head == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	while (new->str != '\0')
-                i++;
 	if (new->str == NULL)
+        {
+                free(new);
+                return (NULL);
+        }
+	while (str[i] != '\0')
 	{
-		free(new);
-		return (NULL);
+		i++;
 	}
 	new->len = i;
 	new->next = *head;
 	*head = new;
-	return (new);
+	return (*head);
 }
