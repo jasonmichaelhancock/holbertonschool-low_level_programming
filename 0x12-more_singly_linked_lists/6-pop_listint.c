47cedf591b6ec2c2be98508aa6f1b18abc8c3728
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-07 06:28:42 +0000

    task6

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - delete head node and return value
+ * @head: the pointer to a pointer.
+ *
+ * Return: the deleted node value..
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *temp;
+
+	if (head == NULL)
+		return(0);
+	i = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return(i);
+}
