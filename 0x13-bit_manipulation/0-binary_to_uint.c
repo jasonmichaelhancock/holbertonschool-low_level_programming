90dc1e0f51bc8695e208f5aa24719248438849a1
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-09 01:30:19 +0000

    task0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimalNumber = 0, i = 0, remainder;
-	unsigned int n;
+	unsigned int num, i, binary_val, decimal_val = 0, base = 1, rem;
 
-	n = atoi(b);
-	while (n != 0)
+	if (b == NULL)
+                return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		remainder = n % 10;
-		n /= 10;
-		decimalNumber += remainder * pow(2, i);
-		++i;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-	return decimalNumber;
+	binary_val = atoi(b);
+	num = binary_val;
+	while (num > 0)
+	{
+		rem = num % 10;
+		decimal_val = decimal_val + rem * base;
+		num = num / 10 ;
+		base = base * 2;
+	}
+	return (decimal_val);
 }
