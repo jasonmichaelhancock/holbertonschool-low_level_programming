d33f94129af98f3ebbe292997d0b5f7faa7fb907
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-10 01:25:47 +0000

    task4

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * clear_bit - translatesunsigned int to binary
+ * @n: the number.
+ * @index: the bit to replace
+ *
+ * Return: The nothing
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int c, k, x;
+
+	c = 1;
+	x = 0;
+
+	while (x < index)
+	{
+		c = 2 * c;
+		x++;
+	}
+	if (*n == 0)
+		return (1);
+	k = *n >> (index);
+	if (k & 0)
+	{
+		return (1);
+	}
+	else
+	{
+		*n = *n - c;
+		return (1);
+	}
+	return (-1);
+}
