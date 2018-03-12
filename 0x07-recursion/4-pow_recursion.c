b4fc645e46139ce9b56b3143df88853eb7484374
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-12 05:19:28 +0000

    task4

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Return the power of a number
+ * @x: the number..
+ * @y: The power.
+ *
+ * Return: the result.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y != 0)
+		return (x * _pow_recursion(x, (y - 1)));
+	else
+		return (1);
+}
