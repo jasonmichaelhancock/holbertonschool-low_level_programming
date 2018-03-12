361064125f8d1137309f4f32f044b738e90f42cc
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-12 02:26:31 +0000

    task0

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,28 +1,17 @@
 #include "holberton.h"
-int _strlen(char *s);
 /**
  * _puts_recursion - prints amstring without loops
- * pointer to the string..
+ * @s: pointer to the string..
  *
  * Return: void.
  */
 void _puts_recursion(char *s)
 {
-	if (_strlen(s) == 0)
+	if (s[0] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(s[0]);
 	_puts_recursion(s + 1);
-}
-int _strlen(char *s)
-{
-	int length;
-
-	for (length = 0; s[length] != '\0'; length++)
-	{
-
-	}
-	return (length);
 }
