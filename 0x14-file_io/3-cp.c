e911cffd261e87431147aa430a5709f1c71fb823
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-17 01:15:55 +0000

    task3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -23,37 +23,37 @@ int main(int argc, char **argv)
 	a = open(argv[1], O_RDONLY);
 	if (a == -1)
 	{
-                dprintf(STDERR_FILENO, "Error0: Can't read from the file %s\n", argv[1]);
+                dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
 	}
 	b = open(argv[2], O_RDWR | O_TRUNC | O_CREAT | O_APPEND, 0664);
 	if (b == -1)
 	{
-		dprintf(STDERR_FILENO, "Error1: Can't write to the file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[2]);
 	}
 	while (not_done == 1)
 	{
 		c = read(a, buf, 1024);
 		if (c == -1)
 		{
-			dprintf(STDERR_FILENO, "Error2: Can't write to the file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[1]);
 		}
 		if (c < 1024)
 			not_done = 0;
 		d = write(b, buf, c);
 		if (d == -1)
 		{
-			dprintf(STDERR_FILENO, "Error3: Can't write to the file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[2]);
 		}
 	}
 	c = close(a);
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error4: Can't close fd %d\n", c);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
 	}
 	d = close(b);
 	if (d == -1)
 	{
-		dprintf(STDERR_FILENO, "Error5: Can't close fd %d\n", c);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
 	}
 	return (0);
 }
