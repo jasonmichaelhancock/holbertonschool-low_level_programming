030f93382d298158345aa55073c3a766bddaa3b4
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-17 01:23:30 +0000

    task3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -7,9 +7,11 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - This is the start
+ * @argc: the number of argments.
+ * @argv: the arguments.
  *
- * Return: Always 0.
+ * Return: 0.
  */
 int main(int argc, char **argv)
 {
@@ -17,43 +19,29 @@ int main(int argc, char **argv)
 	char buf[1024];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[0], argv[1]);
-	}
 	a = open(argv[1], O_RDONLY);
 	if (a == -1)
-	{
-                dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
 	b = open(argv[2], O_RDWR | O_TRUNC | O_CREAT | O_APPEND, 0664);
 	if (b == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[2]);
-	}
 	while (not_done == 1)
 	{
 		c = read(a, buf, 1024);
 		if (c == -1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[1]);
-		}
 		if (c < 1024)
 			not_done = 0;
 		d = write(b, buf, c);
 		if (d == -1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to the file %s\n", argv[2]);
-		}
 	}
 	c = close(a);
 	if (c == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
-	}
 	d = close(b);
 	if (d == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c);
-	}
 	return (0);
 }
