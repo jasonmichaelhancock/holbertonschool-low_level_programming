d7fa51812655e5064f60a1b7f02529db98eb559b
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-18 02:08:37 +0000

    CORRECT4

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	readfile = read(newfile, buf, letters);
 	if (readfile == -1)
 		return (0);
-	writefile = write(STDOUT_FILENO, buf, letters);
+	writefile = write(STDOUT_FILENO, buf, readfile);
 	if (writefile == -1)
 		return (0);
 	free(buf);
