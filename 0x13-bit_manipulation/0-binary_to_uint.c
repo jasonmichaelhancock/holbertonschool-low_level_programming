192188e61c14c212dab64a5df7df90032caa1938
Jason Hancock <jasonmichaelhancock@gmail.com>
2018-03-18 03:20:19 +0000

    task0correction

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i, j, value = 0, base = 1, digit;
 
 	if (b == NULL)
-                return (0);
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 	for (j = 0; j < i; j++)
-        {
+	{
 		digit = ((b[i - j - 1]) - 48);
 		value = value + (digit * base);
 		base = base * 2;
