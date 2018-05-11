f5f49b480cdef536de136fe6c77546ead76021a5
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-11 23:30:41 +0000

    task0

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+      hash_table_t *hash;
+      unsigned long int i = 0;
+
+      hash = malloc(sizeof(hash_table_t));
+      if(hash == NULL)
+      {
+	    return(NULL);
+      }
+      hash->size = size;
+      hash->array = malloc(sizeof(hash_node_t *) * size);
+      if(hash->array == NULL)
+      {
+	    free(hash);
+	    return(NULL);
+      }
+      while(i < size)
+      {
+	    hash->array[i] = NULL;
+	    i++;
+      }
+      return(hash);
+}
