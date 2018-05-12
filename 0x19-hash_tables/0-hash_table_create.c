08d6b3ee9cb8e0c4139d38f37b3617b4843afa04
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-12 00:55:29 +0000

    correct betty

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,27 +2,33 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ * @size: the length of the array.
+ *
+ * Return: address of hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-      hash_table_t *hash;
-      unsigned long int i = 0;
+hash_table_t *hash;
+unsigned long int i = 0;
 
-      hash = malloc(sizeof(hash_table_t));
-      if(hash == NULL)
-      {
-	    return(NULL);
-      }
-      hash->size = size;
-      hash->array = malloc(sizeof(hash_node_t *) * size);
-      if(hash->array == NULL)
-      {
-	    free(hash);
-	    return(NULL);
-      }
-      while(i < size)
-      {
-	    hash->array[i] = NULL;
-	    i++;
-      }
-      return(hash);
+hash = malloc(sizeof(hash_table_t));
+if (hash == NULL)
+{
+return (NULL);
+}
+hash->size = size;
+hash->array = malloc(sizeof(hash_node_t *) * size);
+if (hash->array == NULL)
+{
+free(hash);
+return (NULL);
+}
+while (i < size)
+{
+hash->array[i] = NULL;
+i++;
+}
+return (hash);
 }
