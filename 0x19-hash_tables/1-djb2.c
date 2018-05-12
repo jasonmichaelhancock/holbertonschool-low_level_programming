ae6eb386478e233d4e3e929e1b9ce5382c7d4fa0
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-12 01:04:14 +0000

    correct for betty

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,12 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - create a hash table
+ * @str: the string to hash.
+ *
+ * Return: the hash ley.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-      unsigned long int hash;
-      int c;
+unsigned long int hash;
+int c;
 
-      hash = 5381;
-      while ((c = *str++))
-      {
-	    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-      }
-      return (hash);
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash)+c; /* hash * 33 + c */
+}
+return (hash);
 }
