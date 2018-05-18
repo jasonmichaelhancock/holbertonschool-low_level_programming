49bf09bb6ecbd9194add14458e813f80b248203b
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-18 21:45:11 +0000

    task3 corrections

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -39,7 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(new)
+		free(new);
 		return (0);
 	}
 	new->key = strdup(key);
