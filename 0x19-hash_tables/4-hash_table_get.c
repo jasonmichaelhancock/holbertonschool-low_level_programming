6693c0c76795e8b76dfb5fafe63f2f4265c34e6a
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-18 22:10:36 +0000

    task4 correction

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -13,9 +13,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 
 	if (ht == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = (ht->array[index]);
 	if (ht->array[index] != NULL)
