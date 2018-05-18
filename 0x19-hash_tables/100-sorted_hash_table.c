70b4152418babc589b30a312b81994227d02c9f6
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-05-18 23:49:16 +0000

    advanced 100

--- 0x19-hash_tables/100-sorted_hash_table.c
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -96,7 +96,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 						printf(", ");
 					}
                                         printf("'%s': '%s'", temp->key, temp->value);
-					temp = temp->prev;
+					temp = temp->sprev;
 					i = 1;
 				}
                         }
