bf498461a8b72aac7196cb6551be8a7b848edef5
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-30 00:15:19 +0000

    betty

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - sort according to size
+ * @array: the set of numbers
+ * @size: the number of numbers
+ *
+ * Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
 unsigned int i, j;
@@ -14,6 +21,14 @@ swap(&array[j], &array[j + 1]);
 print_array(array, size);
 }
 }
+
+/**
+ * swap - switch numbers
+ * @xp: 1st number
+ * @yp: 2nd number
+ *
+ * Return: Always 0
+ */
 void swap(int *xp, int *yp)
 {
 int temp = *xp;
