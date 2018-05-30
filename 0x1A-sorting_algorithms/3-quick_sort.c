2b6e101382366e0137e398bca7a74cfe6beab554
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-05-30 22:03:58 +0000

    revise task3

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -11,41 +11,70 @@ void quick_sort(int *array, size_t size)
 {
 	qksort(array, 0, size - 1, size);
 }
+
 /**
- * quick_sort - sort according to size
- * @first: the set of numbers
- * @last: the number of numbers
+ * qk_sort - sort according to size
+ * @array: the set of numbers
+ * @low: the low index
+ * @high: the high index
+ *
+ * Return: the index
+ */
+int partition (int *array, int low, int high)
+{
+	int pivot = array[high];
+	int i = (low - 1);
+	int j;
+
+	for (j = low; j <= high- 1; j++)
+	{
+		/* If current element is smaller than or
+		   equal to pivot */
+		if (array[j] <= pivot)
+		{
+			i++;    /* increment index of smaller element */
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+/**
+ * qk_sort - sort according to size
+ * @array: the set of numbers
+ * @low: the low index
+ * @high: the high index
+ * @size: the size
  *
  * Return: Nothing
  */
-void qksort(int *array, int first, int last, size_t size)
+void qksort(int *array, int low, int high, size_t size)
 {
-	int i, j, pivot, temp;
-
-	if(first<last)
+	if (low < high)
 	{
-		pivot=last;
-		i=first;
-		j=last;
+		/* pi is partitioning index, arr[p] is now
+		   at right place */
+		int pi = partition(array, low, high);
 
-		while(i<j)
-		{
-			while(array[i] <= array[pivot] && i < last)
-				i++;
-			while(array[j] > array[pivot])
-				j--;
-			if(i < j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
+		/* Separately sort elements before
+		   partition and after partition */
 		print_array(array, size);
-		temp = array[pivot];
-		array[pivot] = array[j];
-		array[j] = temp;
-		qksort(array, first, j - 1, size);
-		qksort(array, j + 1, last, size);
+		qksort(array, low, pi - 1, size);
+		qksort(array, pi + 1, high, size);
 	}
+}
+
+/**
+ * swap - switch numbers
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: Always 0
+ */
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
