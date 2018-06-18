4914ba34f04cdad97aeca4c9062a39f08a5ed315
jasonmichaelhancock <34435212+jasonmichaelhancock@users.noreply.github.com>
2018-06-18 14:43:31 -0700

    Update 5-island_perimeter.py

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -4,8 +4,9 @@ def island_perimeter(grid):
     """Function finds the perimeter of an island."""
     per = 0
     x = len(grid)
+    y = len(grid[0])
     for i in range(x):
-        for j in range(x):
+        for j in range(y):
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0:
                     per = per + 1
