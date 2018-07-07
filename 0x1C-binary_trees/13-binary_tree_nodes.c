cfd516eacdde42c37058c91c1c076937ca830b3a
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-07 06:55:28 +0000

    task13

---
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - find nodes with children count of tree
+ *
+ * @tree: pointer to the root
+ *
+ * Return: number of leaves
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return(0);
+if (tree->left == NULL && tree->right == NULL)
+return(0);
+else
+return(binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
