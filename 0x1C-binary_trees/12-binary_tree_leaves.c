3e4530d8ef4785e34cad93af499f6a61ca67b84d
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-07 06:46:22 +0000

    task12 betty

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leave - find leaf count of tree
+ * binary_tree_leaves - find leaf count of tree
  *
  * @tree: pointer to the root
  *
@@ -9,10 +9,10 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return(0);
-	if (tree->left == NULL && tree->right == NULL)
-		return(1);
-	else
-		return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+if (tree == NULL)
+return(0);
+if (tree->left == NULL && tree->right == NULL)
+return(1);
+else
+return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
