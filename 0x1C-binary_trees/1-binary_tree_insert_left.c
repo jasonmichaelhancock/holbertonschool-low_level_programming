93c8b8951103cba0741e0ec2e10db43d8dd00a81
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-07 03:26:46 +0000

    task1 revise

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -9,6 +9,8 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
   binary_tree_t *new_node;
+  if (parent == NULL)
+    return(NULL);
   new_node = malloc(sizeof(binary_tree_t));
   if (new_node == NULL)
     return(NULL);
@@ -17,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
   if (parent->left == NULL)
     {
       parent->left = new_node;
+      new_node->parent = parent;
       new_node->left = NULL;
       new_node->right = NULL;
     }
