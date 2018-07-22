31a9707f075e36e6345e5874fa7e18930cc52f6b
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-22 00:36:40 +0000

    task14

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rheight;
 	size_t lheight;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return(0);
 	lheight = binary_tree_height(tree->left);
 	rheight = binary_tree_height(tree->right);
