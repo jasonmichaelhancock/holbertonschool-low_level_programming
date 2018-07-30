ec0f992250d734c0ed4af0356cf62246b2d56003
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-30 08:15:00 +0000

    revised h file

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct dlistint_s
   struct dlistint_s *prev;
   struct dlistint_s *next;
 } dlistint_t;
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
